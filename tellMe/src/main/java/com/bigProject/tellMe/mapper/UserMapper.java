088ba3bc9c960961eac59470eb9fd4df3df5e180
taesik <0114mts@gmail.com>
2025-01-24 16:38:18 +0900

    complete login

--- tellMe/src/main/java/com/bigProject/tellMe/mapper/UserMapper.java
+++ tellMe/src/main/java/com/bigProject/tellMe/mapper/UserMapper.java
@@ -6,5 +6,6 @@ import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+    UserDTO userToUserDTO(User user);
     User userDTOToUser(UserDTO userDTO);
 }
--- tellMe/src/main/java/com/bigProject/tellMe/repository/UserRepository.java
+++ tellMe/src/main/java/com/bigProject/tellMe/repository/UserRepository.java
@@ -2,6 +2,10 @@ package com.bigProject.tellMe.repository;
 
 import com.bigProject.tellMe.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUserId(String userId);
@@ -9,4 +13,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
     User findByPhone(String phoneNum);
 
     User findByEmail(String eMail);
+
+    List<User> findByUserName(String userName);
+
+    @Query("SELECT u FROM User u WHERE u.userName = :userName AND u.phone = :phone")
+    List<User> findByUserNameAndPhone(@Param("userName") String userName, @Param("phone") String phone);
 }
