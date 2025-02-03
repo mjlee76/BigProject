550bd46815d6582c3d1fb1295f2b58e549cc0a97
atildayear <yeonah.park97@gmail.com>
2025-02-03 11:23:09 +0900

    Backup: Local changes before pulling latest code

---
+++ tellMe/src/main/java/com/bigProject/tellMe/entity/Report.java
@@ -0,0 +1,38 @@
+package com.bigProject.tellMe.entity;
+
+import com.bigProject.tellMe.enumClass.Category;
+import com.bigProject.tellMe.enumClass.ReportStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Builder // notice
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class Report {
+
+    @Id @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "report_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String report; //파일 경로를 여기에 저장
+
+    @Enumerated(EnumType.STRING) // ✅ ENUM을 문자열로 저장
+    @Column(nullable = false)
+    private Category category;//보고서 유형
+
+    @Enumerated(EnumType.STRING) // ✅ ENUM을 문자열로 저장
+    @Column(nullable = false)
+    private ReportStatus reportStatus;
+
+    @Column(nullable = false)
+    private LocalDateTime createDate;
+
+}
---
+++ tellMe/src/main/java/com/bigProject/tellMe/repository/ReportRepository.java
@@ -0,0 +1,9 @@
+package com.bigProject.tellMe.repository;
+
+import com.bigProject.tellMe.entity.Report;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReportRepository extends JpaRepository<Report, Long> {
+}
