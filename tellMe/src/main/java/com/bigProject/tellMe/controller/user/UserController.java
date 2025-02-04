e1cf1c92cee7d2277943eb5a8731460d0925ccfd
park <yis123456@naver.com>
2025-02-04 13:38:09 +0900

    내 페이지 수정.

--- tellMe/src/main/java/com/bigProject/tellMe/controller/user/UserController.java
+++ tellMe/src/main/java/com/bigProject/tellMe/controller/user/UserController.java
@@ -31,11 +31,11 @@ public class UserController {
         //userDTO.setRole(Role.ROLE_ADMIN);
 //        userDTO.setRole(Role.ROLE_MANAGER);
         //userDTO.setRole(Role.ROLE_COUNSELOR);
-        userDTO.setRole(Role.ROLE_USER);
-        userDTO.setCount(0);
-        userDTO.setCreateDate(LocalDateTime.now());
-        System.out.println("======================================");
-        System.out.println(userDTO.toString());
+        //userDTO.setRole(Role.ROLE_USER);
+//        userDTO.setCount(0);
+//        userDTO.setCreateDate(LocalDateTime.now());
+//        System.out.println("======================================");
+        //System.out.println(userDTO.toString());
         userService.save(userDTO);
         return "login";
     }
