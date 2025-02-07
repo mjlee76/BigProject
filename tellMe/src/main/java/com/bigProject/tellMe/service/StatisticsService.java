fd1d9bf6e395a853d059c6d151c912095778b4c0
mjlee76 <dlalswns0422@gmail.com>
2025-02-07 17:22:11 +0900

    문의게시판 시작하기 버튼 추가

--- tellMe/src/main/java/com/bigProject/tellMe/service/StatisticsService.java
+++ tellMe/src/main/java/com/bigProject/tellMe/service/StatisticsService.java
@@ -1,6 +1,7 @@
 package com.bigProject.tellMe.service;
 
 import com.bigProject.tellMe.dto.StatisticsDTO;  // 통계 DTO
+import com.bigProject.tellMe.enumClass.Category;
 import com.bigProject.tellMe.enumClass.ReportStatus;
 import com.bigProject.tellMe.enumClass.Status;
 import lombok.RequiredArgsConstructor;
@@ -23,6 +24,20 @@ public class StatisticsService {
         // 보고서 상태별 통계
         long 미확인Count = reportService.countReportsByStatus(ReportStatus.미확인);
         long 확인완료Count = reportService.countReportsByStatus(ReportStatus.확인완료);
+
+//        // 민원 카테고리별 통계 - 리턴값 넣기
+//        long 모욕Count = questionService.countByCategory(Category.모욕);
+//        long 협박Count = questionService.countByCategory(Category.협박);
+//        long 욕설Count = questionService.countByCategory(Category.욕설);
+//        long 인종Count = questionService.countByCategory(Category.인종차별);
+//        long 성차Count = questionService.countByCategory(Category.성차별);
+//        long 성희Count = questionService.countByCategory(Category.성희롱);
+//        long 장애Count = questionService.countByCategory(Category.장애인차별);
+//        long 혐오Count = questionService.countByCategory(Category.혐오발언);
+//        long 종교Count = questionService.countByCategory(Category.종교차별);
+//        long 외모Count = questionService.countByCategory(Category.외모차별);
+//        long 지역Count = questionService.countByCategory(Category.지역차별);
+//        long 나이Count = questionService.countByCategory(Category.나이차별);
 
         return new StatisticsDTO(접수중Count, 처리중Count, 답변완료Count, 미확인Count, 확인완료Count);
     }
