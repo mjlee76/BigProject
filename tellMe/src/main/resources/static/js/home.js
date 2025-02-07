6c93a2c853cc6b39ee54ae925c85a9421610ada4
park <yis123456@naver.com>
2025-02-07 13:50:05 +0900

    내 페이지 수정.

--- tellMe/src/main/resources/static/js/home.js
+++ tellMe/src/main/resources/static/js/home.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+<<<<<<< HEAD
     let currentIndex = 0;
     const slider = document.querySelector(".slider");
     const slides = document.querySelectorAll(".slide");
@@ -64,3 +65,17 @@ document.addEventListener("DOMContentLoaded", () => {
     // 웹 시작 시 첫 번째 버튼 활성화
     updateActiveButton();
 });
+=======
+    // Home Section 자동 슬라이드
+    let currentIndex = 0;
+    const slides = document.querySelector(".slider");
+    const totalSlides = document.querySelectorAll(".slide").length;
+
+    function moveSlide() {
+        currentIndex = (currentIndex + 1) % totalSlides;
+        slides.style.transform = `translateX(-${currentIndex * 100}vw)`;
+    }
+
+    setInterval(moveSlide, 4000); // 3초마다 슬라이드 이동
+});
+>>>>>>> 882e045 (내 페이지 수정.)
